d015bb83b161bf7b8edd95f9512e3e679ee94abd
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-03-27 17:41:54 +0900

    사라진 a 태그 추가

--- _11_MemberAddServlet_Refresh/src/main/java/spms/sevlets/MemberListServlet.java
+++ _11_MemberAddServlet_Refresh/src/main/java/spms/sevlets/MemberListServlet.java
@@ -22,7 +22,7 @@ public class MemberListServlet extends GenericServlet{
 		Connection conn = null;			// DB 서버와의 연결 객체
 		Statement stmt = null;			// sql문
 		ResultSet rs = null;			// Select문의 결과
-		
+
 		try {
 			// 메모리에 클래스 로딩
 			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
@@ -32,21 +32,32 @@ public class MemberListServlet extends GenericServlet{
 					"study");								// password
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery("SELECT mno, mname, email, cre_date" +
-								   " FROM members" +
-					   	  		   " ORDER BY mno ASC");
+								 " FROM members" +
+								 " ORDER BY mno ASC");
+
 			res.setContentType("text/html;charset=UTF-8");
 			PrintWriter out = res.getWriter();
-			out.println("<html><head><title>회원 목록</title><head>");
+			out.println("<html><head><title>회원 목록</title></head>");
 			out.println("<body><h1>회원 목록</h1>");
+			/*신규회원 추가*/
+			/*
+			 * href = '/add' => 절대경로
+			 *  localhost:9999/<contextRoot>/add
+			 * 
+			 * href = 'add'  => 상대경로
+			 *  localhost:9999/<contextRoot>/member/add
+			 * */
+			out.println("<p><a href='add'>신규 회원</a></p>");
 			while(rs.next()) {
 				out.println(
-						rs.getInt("mno") + ", " +
-					    rs.getString("mname") + ", " +
-					    rs.getString("email") + ", " +
-					    rs.getDate("cre_date") + "<br>"
+						rs.getInt("mno") + ", " + 
+						rs.getString("mname") + ", " +
+						rs.getString("email") + ", " +
+						rs.getDate("cre_date") + "<br>"
 						);
 			}
 			out.println("</body></html>");
+
 		}catch(Exception e) {
 			throw new ServletException(e);
 		}finally {
