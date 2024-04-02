c05035c7a1794f05a52efb5266cc5cd5d53b665b
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-04-02 14:35:40 +0900

    49 controller 인터페이스 정의 기존 servlet page constroller를 pojo방식의  page controller로 정의

--- _49_PgaeController_Pojo/src/main/java/spms/util/DBConnectionPool.java
+++ _49_PgaeController_Pojo/src/main/java/spms/util/DBConnectionPool.java
@@ -1,4 +1,4 @@
- package spms.util;
+package spms.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,9 +12,7 @@ public class DBConnectionPool {
 	String url;
 	String username;
 	String password;
-	
-	// ArrayList대신 Vector를 변경하다.
-	// ArrayList와 Vector는 동일한 구조이지만, Vector 멀티스레드 동기화 처리가 되어 있다.
+
 	List<Connection> connList = new Vector<Connection>();
 	
 	public DBConnectionPool(String driver, String url,
@@ -36,28 +34,21 @@ public class DBConnectionPool {
 	}
 	
 	public Connection getConnection() throws Exception{
-		// 현재 사용하지 않고 있는 Connection 객체가 존재한다면
 		if(connList.size() > 0) {			
-			// 첫번째 인덱스에 위치한 Connection 객체를 꺼낸다.
 			Connection conn = connList.remove(0);
-			// 서버와 통신이 연결되어 있다면
 			if(conn.isValid(10)) {
 				return conn;
 			}
 		}
-		
-		// 위의 return conn;이 되지 않았다는 것은 Connection객체가 없거나, 정상적이지 않다는
-		// 뜻이므로 새롭게 서버와 연결된 Connection 객체를 생성해서 return한다.
+
 		return DriverManager.getConnection(url, username, password);
 	}
 	
 	public void returnConnection(Connection conn) throws Exception{
-		// 사용을 마친 정상적인 Connection 객체를 다시 connList에 저장한다.
 		if(conn != null && conn.isValid(10))
 			connList.add(conn);
 	}
 	
-	// 모든 Connection 객체의 연결을 종료한다.
 	public void closeAll() {
 		for(Connection conn : connList)
 			try {conn.close();} catch(Exception e) {}
