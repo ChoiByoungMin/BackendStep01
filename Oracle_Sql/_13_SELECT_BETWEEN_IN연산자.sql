d8193173f06dfbca766a9442a7ae79e424d8c7f6
DESKTOP-1L3HC4J\Lenovo <cbm1801@naver.com>
2024-04-04 16:27:03 +0900

    12번 문제 풀이 13번 between, in 연산자

--- Oracle_Sql/_13_SELECT_BETWEEN_IN연산자.sql
+++ Oracle_Sql/_13_SELECT_BETWEEN_IN연산자.sql
@@ -6,12 +6,21 @@
 --2) 급여가 2000에서 1000사이 사원
 --작은 조건이 반드시 먼저 나와야 한다
  -- 검색이 안된다
-
+SELECT *
+FROM emp
+WHERE sal BETWEEN 2000 AND 1000;
 
 --3) 10, 20번 부서 사원
+SELECT *
+FROM emp
+WHERE eno BETWEEN '10' AND '20'
+ORDER BY dno;
 
 
-
+SELECT *
+FROM emp
+WHERE dno='10' OR dno='20'
+ORDER BY dno;
 
 --4) 1992년에서 1996년 사이에 입사한 사원을 검색하세요
 '1992/01/01' => '1992/01/01:00:00:00'
@@ -19,7 +28,13 @@
 만약 1996년 12월 31일 11시 20분에 30초에 입사한 사원은 검색에서 제외
 되는 결과가 나타난다
 
+-- 날짜 검색시 날짜 포맷의 변경이 필요하다.
 
+ALTER SESSION SET nls_date_format='YYYY/MM/DD';
+
+SELECT *
+FROM emp
+WHERE hdate BETWEEN '1992/01/01' AND '1996/12/31';
 
 
 
@@ -28,13 +43,26 @@
 
 --정확한 비교를 위해 아래처럼 한다
 
+ALTER SESSION SET nls_date_format='YYYY/MM/DD:HH24:MI:SS';
 
+SELECT *
+FROM emp
+WHERE hdate BETWEEN '1992/01/01:00:00:00' AND '1996/12/31:23:59:59';
+
+-- 함수를 사용하는 방법이 있다.
 
 [IN 연산자]
 
 --5) 10, 20번 부서 사원
 
+SELECT *
+FROM emp
+WHERE dno IN('10','20')
+ORDER BY dno;
 
 --6) 개발이나 회계 업무를 담당하는 사원
-
+SELECT *
+FROM emp
+WHERE job IN('개발','회계')
+ORDER BY job;
 
